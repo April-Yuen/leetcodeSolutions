6211b725cd18d38afa2a4e72d757e759979f1472
April Yuen <april.you@gmail.com>
2022-01-01 21:02:46 -0500

    Adding a leetcode easy review of two pointer.

---
+++ remove-elements-review.js
@@ -0,0 +1,34 @@
+// 27. Remove Element
+
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+// Return k after placing the final result in the first k slots of nums.
+
+// Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+// Very basic brute force that does not meet time complexity is:
+// function removeElement(nums, val){
+//     return nums.filter(num => num !== val).length
+// }
+
+
+// Two pointer technique, 1 fast, 1 slow. 
+// set a variable to 0. 
+// make a for loop and iterate through to find the nums that are not equal to val. 
+// if the num is not equal to val, then replace nums[i] with nums[j].
+// return j
+function removeElement(nums, val){
+    let j = 0
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] !== val){
+            nums[j] = nums[i]
+            j++
+        } 
+    }
+    return j
+}
+                          
+console.log(removeElement([3,2,2,3], 3))
+console.log(removeElement([0,1,2,2,3,0,4,2], 2))
