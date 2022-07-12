88e24980d6b14e8532a23544377c9d4f8a2bbdc6
April Yuen <april.you@gmail.com>
2022-07-12 09:11:00 -0500

    Added changes to valid-parentheses.js

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -21,3 +21,24 @@
 // Input: s = "(]"
 // Output: false
 
+var isValid = function(s) {
+    let map = new Map();
+    const stack = []
+    
+    map.set(")", "(")
+    map.set("}", "{")
+    map.set("]", "[")
+    
+    for(let i = 0; i < s.length; i++){
+        const curr = s[i]
+        const last = stack[stack.length-1]
+        
+        if(map.has(curr) && map.get(curr)===last){
+            stack.pop()
+        }else{
+            stack.push(curr)
+        }
+    }
+    return stack.length === 0
+ };
+
