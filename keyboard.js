eea24e63a6eab91d167ba73ce2ff3e65c67b4d38
April Yuen <april.you@gmail.com>
2022-09-11 11:36:25 -0400

    Adding an easy solution.

---
+++ keyboard.js
@@ -0,0 +1,26 @@
+
+
+
+
+function findwords (words) {
+    const row1 = new Set("qwertyuiop");
+    const row2 = new Set("asdfghjkl");
+    const row3 = new Set("zxcvbnm");
+
+    const res = [];
+    
+    for (const word of words) {
+        if (canBeTyped(word, row1) || canBeTyped(word, row2) || canBeTyped(word, row3)) res.push(word);
+    }
+    
+    return res;
+    
+    function canBeTyped(word, row) {
+       for (const char of word) {
+           if (!row.has(char.toLowerCase())) return false;
+       }
+       return true;
+    }
+};
+
+console.log(findwords(["Hello","Alaska","Dad","Peace"]))
