8bf0f287067c50c44a07b41169c31f2e9381436e
April Yuen <april.you@gmail.com>
2023-02-07 20:15:13 -0500

    Adding a review problem for stack.

---
+++ longStrReview.js
@@ -0,0 +1,38 @@
+// 1047. Remove All Adjacent Duplicates In String
+
+// You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+
+// We repeatedly make duplicate removals on s until we no longer can.
+
+// Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+ 
+
+// Example 1:
+
+// Input: s = "abbaca"
+// Output: "ca"
+// Explanation: 
+// For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+// Example 2:
+
+// Input: s = "azxxzy"
+// Output: "ay"
+
+//  we will have an empty stack. 
+// we will do a for loop and check to see if that char is in the stack. 
+// if it is, we will pop
+// else we will push the char into the stack. 
+// return s.join()
+
+var removeDuplicates = function(s) {
+    let stack = []
+    for(let letter of s){
+        if(letter === stack[stack.length-1]){
+            stack.pop()
+        }else{
+            stack.push(letter)
+        }
+    }
+    return stack.join("")
+};
